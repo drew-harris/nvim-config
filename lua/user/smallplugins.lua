e0358e0becc0e0c5c6964c0ca21a5e7bc13a10d4
Drew Harris <harrisd@smu.edu>
2022-11-25 02:56:18 -0600

    Setup todo-comments in smallplugins.lua

--- lua/user/smallplugins.lua
+++ lua/user/smallplugins.lua
@@ -4,6 +4,10 @@ require("leap").add_default_mappings()
 -- Line peek when typing :<num>
 require("numb").setup()
 
+require("todo-comments").setup({
+	-- Configuration here
+})
+
 -- Smooth scrolling
 require("neoscroll").setup({
 	easing_function = "quadratic",
