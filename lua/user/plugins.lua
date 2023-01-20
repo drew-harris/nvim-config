eecf8ef21acdcb4d2e3fd3de0f16da03a098c35b
Drew Harris <harrisd@smu.edu>
2023-01-19 22:40:32 -0600

    Remove packer dumbness + move quickfix

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -15,13 +15,13 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.cmd([[packadd packer.nvim]])
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
+-- -- Autocommand that reloads neovim whenever you save the plugins.lua file
+-- vim.cmd([[
+--   augroup packer_user_config
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup end
+-- ]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -121,6 +121,11 @@ return packer.startup(function(use)
 	use("folke/neodev.nvim")
 	use("stevearc/aerial.nvim") -- Outline panel
 
+	use({
+		"weilbith/nvim-code-action-menu",
+		cmd = "CodeActionMenu",
+	})
+
 	-- Rust
 	use({ "simrat39/rust-tools.nvim" })
 	use("simrat39/inlay-hints.nvim")
@@ -141,7 +146,7 @@ return packer.startup(function(use)
 		"nvim-treesitter/nvim-treesitter",
 	})
 
-	use("nvim-treesitter/nvim-treesitter-context")
+	--use("nvim-treesitter/nvim-treesitter-context")
 
 	-- Git
 	use({ "lewis6991/gitsigns.nvim" })
--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -146,7 +146,8 @@ local mappings = {
 
 	l = {
 		name = "LSP",
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+		--a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+		a = { "<cmd>CodeActionMenu<cr>", "Code Action" },
 		d = {
 			"<cmd>Telescope diagnostics bufnr=0<cr>",
 			"Document Diagnostics",
