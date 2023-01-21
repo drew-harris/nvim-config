9f04f59f493bbfd8d51e53cb19971c7193101c45
Drew Harris <dsharris10@gmail.com>
2023-01-21 13:02:44 -0600

    AUTO LSP SETUP WITH MASON!!!!

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -47,39 +47,41 @@ local function organize_imports()
 	vim.lsp.buf.execute_command(params)
 end
 
-for _, server in pairs(servers) do
-	opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-		commands = {
-			OrganizeImports = {
-				organize_imports,
-				description = "Organize Imports",
+require("mason-lspconfig").setup_handlers({
+	function(server)
+		opts = {
+			on_attach = require("user.lsp.handlers").on_attach,
+			capabilities = require("user.lsp.handlers").capabilities,
+			commands = {
+				OrganizeImports = {
+					organize_imports,
+					description = "Organize Imports",
+				},
 			},
-		},
-	}
+		}
 
-	---@diagnostic disable-next-line: missing-parameter  From initial commit
-	server = vim.split(server, "@")[1]
+		---@diagnostic disable-next-line: missing-parameter  From initial commit
+		server = vim.split(server, "@")[1]
 
-	local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-	if require_ok then
-		opts = vim.tbl_deep_extend("force", conf_opts, opts)
-	end
+		local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
+		if require_ok then
+			opts = vim.tbl_deep_extend("force", conf_opts, opts)
+		end
 
-	if server == "sumneko_lua" then
-		require("neodev").setup({
-			-- Options for neodev
-		})
-	end
+		if server == "sumneko_lua" then
+			require("neodev").setup({
+				-- Options for neodev
+			})
+		end
 
-	if server == "denols" then
-		opts.root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
-	end
+		if server == "denols" then
+			opts.root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc")
+		end
 
-	if server == "tsserver" then
-		opts.root_dir = lspconfig.util.root_pattern("package.json")
-	end
+		if server == "tsserver" then
+			opts.root_dir = lspconfig.util.root_pattern("package.json")
+		end
 
-	lspconfig[server].setup(opts)
-end
+		lspconfig[server].setup(opts)
+	end,
+})
