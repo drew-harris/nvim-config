5fdf91578166bb6f1330956852a7629025f7cdb8
Drew Harris <harrisd@smu.edu>
2024-01-17 21:28:37 -0600

    Mini files

--- lua/user/plugins/mini.lua
+++ lua/user/plugins/mini.lua
@@ -6,9 +6,9 @@ return {
 				use_as_default_explorer = true,
 			},
 		})
-		-- vim.keymap.set("n", "<leader>e", function()
-		-- 	require("mini.files").open()
-		-- end, { desc = "Mini File" })
+		vim.keymap.set("n", "<leader>E", function()
+			require("mini.files").open()
+		end, { desc = "Mini File" })
 
 		require("mini.ai").setup()
 	end,
--- test.lua
+++
@@ -1,8 +0,0 @@
-local telescope = require("telescope")
-local fb = telescope.extensions.file_browser
-local picker = fb.file_browser
-
-picker({
-	files = true,
-	grouped = true,
-})
