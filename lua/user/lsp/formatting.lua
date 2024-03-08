57a7566e5787966abdf2700ddf7b9d35f5abb7be
Drew Harris <harrisd@smu.edu>
2024-03-07 23:14:51 -0600

    Ignore java formatting

--- lua/user/lsp/formatting.lua
+++ lua/user/lsp/formatting.lua
@@ -6,12 +6,19 @@ require("conform").setup({
 		go = { "goimports", "gofmt" },
 		-- Use a sub-list to run only the first available formatter
 		javascript = { { "prettierd" } },
+		javascriptreact = { { "prettierd" } },
+		typescript = { { "prettierd" } },
+		typescriptreact = { { "prettierd" } },
 	},
-	format_on_save = {
+	format_on_save = function(bufnr)
 		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
+		local ignore_filetypes = { "java" }
+		if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+			return
+		end
+		return {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		}
+	end,
 })
-
-require("mason-conform").setup()
