343f360e428b3ff7cfae13e6519df499412f2099
Drew Harris <harrisd@smu.edu>
2024-03-17 13:37:24 -0500

    Fix harpoon keymaps

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -21,5 +21,17 @@ return {
 		vim.keymap.set("n", "<leader>hp", ui.nav_prev, { desc = "Nav Next" })
 		vim.keymap.set("n", "<leader>hs", "<cmd>Telescope harpoon marks<cr>", { desc = "Search Marks" })
 		vim.keymap.set("n", "<leader>hc", require("harpoon.cmd-ui").toggle_quick_menu, { desc = "Search Marks" })
+		local keymap = vim.api.nvim_set_keymap
+		local opts = { noremap = true, silent = true }
+
+		for i = 1, 9 do
+			keymap("n", "<leader>" .. i, string.format('<cmd>:lua require("harpoon.ui").nav_file(%d)<cr>', i), opts)
+			keymap(
+				"n",
+				"<leader>h" .. i,
+				string.format('<cmd>:lua require("harpoon.tmux").sendCommand("{last}", %d)<cr>', i),
+				opts
+			)
+		end
 	end,
 }
